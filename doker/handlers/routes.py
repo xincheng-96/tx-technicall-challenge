143c4aeec2f221e4c9a921f4c64a8c3a6331fd41
Xin Cheng <57724013+xincheng-96@users.noreply.github.com>
2022-10-22 20:55:14 +0300

    merge unit tests into main (#2)
    * add unit tests
    
    * add .vs to gitignore
    
    * add comments
    
    * Delete .vs directory
    
    Co-authored-by: Cheng <Xin.Cheng@elekta.com>

---
+++ doker/handlers/routes.py
@@ -0,0 +1,55 @@
+from flask import jsonify
+import logging
+
+
+def configure_routes(app):
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    @app.route('/<ini_string>', methods=['GET'])
+    def transform_string(ini_string):
+        #ini_string is the input string from user
+        #i is encode indicator, which represent how many positions should be added for each charactor
+        i = 5
+
+        logger.info('initial string: %s',ini_string)
+        #char_i means charactor index on ascii standard
+        def transform_with_range(char_i, i, min_i, max_i):
+            adjusted_i = (char_i - min_i + i) % (max_i - min_i + 1) + min_i
+            return chr(adjusted_i)
+
+        def get_char_range(char_i):
+            # capitals
+            if char_i in range(65,91):
+                return 65, 90
+            # lower cases
+            elif char_i in range(97,123):
+                return 97, 122
+            #numbers
+            elif char_i in range(48,58):
+                return 48, 57
+            else: 
+                return (None, None)
+        
+        out_string = ""
+
+        for c in ini_string:
+            char_i = ord(c)
+            # capitals
+            min_i, max_i = get_char_range(char_i)      
+            #if the character is a special character or not defined, do not transfer it
+            if min_i is None or max_i is None:
+                out_string += c
+            else: 
+                out_string += transform_with_range(char_i, i, min_i, max_i)
+        logger.info('encoded string: %s',out_string)
+        return out_string
+
+    @app.route("/status")
+    def get_status():
+        healthStatusReturnBody = [
+            { 'status': 'ok'}
+        ]
+
+
+        return jsonify(healthStatusReturnBody)
---
+++ doker/tests/test_routes.py
@@ -0,0 +1,45 @@
+from flask import Flask
+import pytest
+
+from doker.handlers.routes import configure_routes
+
+@pytest.fixture
+def client():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+
+    return client
+
+    #test status route, should return 200
+def test_status_route(client):
+
+    url = '/status'
+
+    response = client.get(url)
+    assert response.get_data() == b'[{"status":"ok"}]\n'
+    assert response.status_code == 200
+
+    #test encoding function with letters
+def test_encode_letter(client):
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/AbcXyz'
+
+    response = client.get(url)
+    print(response)
+    assert response.get_data() == b'FghCde'
+    assert response.status_code == 200
+
+    #test encoding function with number
+def test_encode_number(client):
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/012789'
+
+    response = client.get(url)
+    print(response)
+    assert response.get_data() == b'567234'
+    assert response.status_code == 200
